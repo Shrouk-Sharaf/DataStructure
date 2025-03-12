941f4c0278f208cebc8402b70fd829c6de0ea256
Shrouk-Sharaf <sayedahmedroka@gmail.com>
2025-03-13 00:02:37 +0200

    uploaded v2 of Bubble, Radix and selection algorithms

--- selection.cpp
+++ selection.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-
-template<typename t>
-void selection_sort(t data[], int n){
+template<typename T>
+void selection_sort(T data[], int n) {
     for (int i = 0; i < n; ++i) {
-        t min = data[i];
+        T min = data[i];
         int index = i;
         for (int j = i; j < n; ++j) {
             if (data[j] < min) {
@@ -13,24 +12,34 @@ void selection_sort(t data[], int n){
                 index = j;
             }
         }
-        t temp = data[i];
+        T temp = data[i];
         data[i] = data[index];
         data[index] = temp;
-    }
 
+        cout << "After iteration " << i + 1 << ": ";
+        for (int k = 0; k < n; ++k) {
+            cout << data[k] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Final sorted array: ";
     for (int i = 0; i < n; ++i) {
         cout << data[i] << " ";
-
     }
-    cout << endl;
+    cout << endl << endl;
 }
 
-int main(){
+int main() {
     int data[] = {2, 8, 1, 9, 7, 3, 4};
-    string names[] = {"hanna","jasmine","Ayat","reham","shrouk", "sayed","nada", "nader","jasmin","raneem"};
-    double foo[] = { 2.2,1.1, 3.3, 4.5, 2.3, 2.22, 1.34, 1.98, 1.03, 1.09};
+    string names[] = {"hanna", "jasmine", "Ayat", "reham", "shrouk", "sayed", "nada", "nader", "jasmin", "raneem"};
+    double foo[] = {2.2, 1.1, 3.3, 4.5, 2.3, 2.22, 1.34, 1.98, 1.03, 1.09};
 
+    cout << "Sorting integers:" << endl;
     selection_sort(data, 7);
+
+    cout << "Sorting strings:" << endl;
     selection_sort(names, 10);
+
+    cout << "Sorting doubles:" << endl;
     selection_sort(foo, 10);
 }
