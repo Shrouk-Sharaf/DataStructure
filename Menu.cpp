c80556fb70ac18bff3dfd0d9484afeb07c220af5
Shrouk-Sharaf <sayedahmedroka@gmail.com>
2025-03-19 01:26:23 +0200

    uploaded v3 of Menu

--- Menu.cpp
+++ Menu.cpp
@@ -1,9 +1,16 @@
 #include "SortingSystem.h"
+#include <type_traits>
 
 template <typename T>
 SortingSystem<T>::SortingSystem(int n) {
     size = n;
     data = new T[size];
+
+    cout << "Enter " << n << " elements of the array:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "Element " << i + 1 << ":\n";
+        cin >> data[i];
+    }
 }
 
 template <typename T>
@@ -13,15 +20,9 @@ SortingSystem<T>::~SortingSystem() {
 }
 
 template <typename T>
-void SortingSystem<T>::setData() {
-    cout << "Enter " << size << " elements: ";
-    for (int i = 0; i < size; i++) {
-        cin >> data[i];
-    }
-}
-
-template <typename T>
 void SortingSystem<T>::selectionSort() {
+    cout << "Initial Data: ";
+    displayData();
     for (int i = 0; i < size - 1; ++i) {
         int minIndex = i;
         for (int j = i + 1; j < size; ++j) {
@@ -32,11 +33,16 @@ void SortingSystem<T>::selectionSort() {
         if (minIndex != i) {
             swap(data[i], data[minIndex]);
         }
+
+        cout << "After iteration " << i + 1 << ": ";
+        displayData();
     }
 }
 
 template <typename T>
 void SortingSystem<T>::bubbleSort() {
+    cout << "Initial Data: ";
+    displayData();
     for (int i = 0; i < size - 1; ++i) {
         bool swapped = false;
         for (int j = 0; j < size - i - 1; j++) {
@@ -45,13 +51,19 @@ void SortingSystem<T>::bubbleSort() {
                 swapped = true;
             }
         }
+
+        cout << "After pass " << i + 1 << ": ";
+        displayData();
+
         if (!swapped) break;
     }
 }
 
-template <typename T>
-void SortingSystem<T>::radixSort() {
-    T maxVal = data[0];
+template<>
+void SortingSystem<int>::radixSort() {
+    cout << "Initial Data: ";
+    displayData();
+    int maxVal = data[0];
     for (int i = 1; i < size; i++) {
         if (data[i] > maxVal) {
             maxVal = data[i];
@@ -59,7 +71,7 @@ void SortingSystem<T>::radixSort() {
     }
 
     for (int exp = 1; maxVal / exp > 0; exp *= 10) {
-        T* output = new T[size];
+        int* output = new int[size];
         int count[10] = {0};
 
         for (int i = 0; i < size; i++)
@@ -75,6 +87,9 @@ void SortingSystem<T>::radixSort() {
 
         for (int i = 0; i < size; i++)
             data[i] = output[i];
+
+        cout << "After sorting by place value " << exp << ": ";
+        displayData();
 
         delete[] output;
     }
@@ -96,19 +111,24 @@ void SortingSystem<T>::showMenu() {
         cout << "\nSelect a sorting algorithm:" << endl;
         cout << "1. Selection Sort" << endl;
         cout << "2. Bubble Sort" << endl;
-        cout << "3. Radix Sort" << endl;
-        cout << "4. Enter new dataset" << endl;
-        cout << "5. Exit" << endl;
-        cout << "Enter your choice (1-5):\n";
+
+        if (is_same<T, int>::value) {
+            cout << "3. Radix Sort" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice:\n";
+        }
+        else {
+            cout << "3. Exit" << endl;
+            cout << "Enter your choice:\n";
+        }
+
         int choice;
         cin >> choice;
 
-        if (choice == 5) {
+        int exitChoice = is_same<T, int>::value ? 4 : 3;
+        if (choice == exitChoice) {
             cout << "Thank you for using the sorting system! Goodbye!" << endl;
             break;
-        } else if (choice == 4) {
-            setData();
-            continue;
         }
 
         switch (choice) {
@@ -121,45 +141,55 @@ void SortingSystem<T>::showMenu() {
                 bubbleSort();
                 break;
             case 3:
-                cout << "Radix Sort..." << endl;
-                radixSort();
+                if (is_same<T, int>::value) {
+                    cout << "Radix Sort..." << endl;
+                    radixSort();
+                }
+                else {
+                    cout << "Invalid choice! Please try again." << endl;
+                }
                 break;
             default:
                 cout << "Invalid choice! Please try again." << endl;
                 continue;
         }
-
-        cout << "Sorted Data: ";
+        cout << "Final Sorted Data: ";
         displayData();
-
-        int option;
-        while (true) {
-            cout << "\n1) Sort again using a different algorithm\n2) Enter a new dataset\n3) Exit\n";
-            cout << "Enter your choice (1-3):\n";
-            cin >> option;
-
-            if (option == 1) {
-                break;
-            }
-            else if (option == 2) {
-                setData();
-                break;
-            }
-            else if (option == 3) {
-                cout << "Thank you for using the sorting system! Goodbye!" << endl;
-                return;
-            }
-            else cout << "Invalid option! Please enter a number between 1 and 3." << endl;
-        }
+        break;
     }
 }
 
 int main() {
-    int n;
-    cout << "Enter the size of the array:\n";
-    cin >> n;
+    int datatype;
+    cout << "Select the data type for the sorting system:" << endl;
+    cout << "1. int" << endl;
+    cout << "2. double" << endl;
+    cout << "3. string" << endl;
+    cout << "Enter your choice:\n";
+    cin >> datatype;
 
-    SortingSystem<int> sorter(n);
-    sorter.setData();
-    sorter.showMenu();
+    int size;
+    cout << "Enter the size of the array:\n";
+    cin >> size;
+
+    switch (datatype) {
+        case 1: {
+            SortingSystem<int> system(size);
+            system.showMenu();
+            break;
+        }
+        case 2: {
+            SortingSystem<double> system(size);
+            system.showMenu();
+            break;
+        }
+        case 3: {
+            SortingSystem<string> system(size);
+            system.showMenu();
+            break;
+        }
+        default:
+            cout << "Invalid choice! Exiting the program." << endl;
+            break;
+    }
 }
