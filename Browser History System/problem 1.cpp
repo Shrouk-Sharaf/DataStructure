416733fc0028ced2e8b88e9cc97e19e7f4b80696
Shrouk-Sharaf <sayedahmedroka@gmail.com>
2025-05-07 02:33:51 +0300

    uploaded v2 of Browser History System

--- Browser History System/problem 1.cpp
+++ Browser History System/problem 1.cpp
@@ -1,32 +1,137 @@
 #include <iostream>
-#include <stack>
 #include <fstream>
 using namespace std;
 
+struct Node {
+    string url;
+    Node* next;
+
+    Node(const string& u) : url(u), next(nullptr) {}
+};
+
+class CustomStack {
+private:
+    Node* top;
+    int size;
+
+public:
+    CustomStack() : top(nullptr), size(0) {}
+
+    ~CustomStack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    void push(const string& url) {
+        Node* newNode = new Node(url);
+        newNode->next = top;
+        top = newNode;
+        size++;
+    }
+
+    string pop() {
+        if (isEmpty()) {
+            return "";
+        }
+        Node* temp = top;
+        string url = temp->url;
+        top = top->next;
+        delete temp;
+        size--;
+        return url;
+    }
+
+    string peek() const {
+        if (isEmpty()) {
+            return "";
+        }
+        return top->url;
+    }
+
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    // For printing the stack contents (in reverse order)
+    void print() const {
+        if (isEmpty()) {
+            cout << "Empty";
+            return;
+        }
+
+        // First, we'll reverse the stack to print it in the correct order
+        Node* current = top;
+        Node* prev = nullptr;
+        Node* next = nullptr;
+
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        // Now print the reversed stack
+        current = prev;
+        while (current != nullptr) {
+            cout << current->url << " ";
+            current = current->next;
+        }
+
+        // Restore the original stack order
+        current = prev;
+        prev = nullptr;
+        next = nullptr;
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+    }
+};
+
 class BrowserHistory {
 private:
-    stack<string> backStack;
-    stack<string> forwardStack;
+    CustomStack backStack;
+    CustomStack forwardStack;
     string currentUrl;
 
 public:
     void visit(const string& url) {
-        while (!forwardStack.empty()) forwardStack.pop();
-        if (!currentUrl.empty()) backStack.push(currentUrl);
+        // Clear forward stack when visiting a new URL
+        while (!forwardStack.isEmpty()) {
+            forwardStack.pop();
+        }
+
+        if (!currentUrl.empty()) {
+            backStack.push(currentUrl);
+        }
         currentUrl = url;
     }
 
     string goBack() {
+        if (backStack.isEmpty()) {
+            return currentUrl; // Can't go back
+        }
+
         forwardStack.push(currentUrl);
-        currentUrl = backStack.top();
-        backStack.pop();
+        currentUrl = backStack.pop();
         return currentUrl;
     }
 
     string goForward() {
+        if (forwardStack.isEmpty()) {
+            return currentUrl; // Can't go forward
+        }
+
         backStack.push(currentUrl);
-        currentUrl = forwardStack.top();
-        forwardStack.pop();
+        currentUrl = forwardStack.pop();
         return currentUrl;
     }
 
@@ -34,27 +139,11 @@ public:
         cout << "Current URL: " << (currentUrl.empty() ? "None" : currentUrl) << endl;
 
         cout << "Back Stack: ";
-        stack<string> temp = backStack, rev;
-        while (!temp.empty()) {
-            rev.push(temp.top());
-            temp.pop();
-        }
-        while (!rev.empty()) {
-            cout << rev.top() << " ";
-            rev.pop();
-        }
+        backStack.print();
         cout << endl;
 
         cout << "Forward Stack: ";
-        temp = forwardStack;
-        while (!temp.empty()) {
-            rev.push(temp.top());
-            temp.pop();
-        }
-        while (!rev.empty()) {
-            cout << rev.top() << " ";
-            rev.pop();
-        }
+        forwardStack.print();
         cout << endl;
 
         cout << "-----------------------------" << endl;
@@ -74,11 +163,11 @@ int main() {
             cout << "Visit --> " << url << endl << endl;
             bh.visit(url);
         }
-        else if (command == "2"){
+        else if (command == "2") {
             cout << "Back --> ";
             cout << bh.goBack() << endl << endl;
         }
-        else if (command == "3"){
+        else if (command == "3") {
             cout << "Forward --> ";
             cout << bh.goForward() << endl << endl;
         }
